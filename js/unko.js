4c9e320fd4349986626884d3e8d3e050f37fc474
PC-PC\PC <pmzzzzx@gmail.com>
2019-09-16 04:21:35 +0900

    糞機能追加

--- js/unko.js
+++ js/unko.js
@@ -2,8 +2,54 @@ const main = document.getElementsByClassName('main');
 let cout = 0;
 main[0].addEventListener('click', function () {
     cout++;
-    if(cout > 1){
-        cout = 0;
-        main[0].style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    if (cout === 1) {
+        let clockz = document.createElement("p");
+        clockz.id = 'clock';
+        document.body.appendChild(clockz);
+        setInterval('clock()', 1000);
+
     }
 }, false);
+
+var OK = true;
+var keyPress = "";
+document.addEventListener('keydown', (event) => {
+    if (cout >= 1) {
+
+        let keyName = event.key;
+        keyPress = keyPress + keyName;
+
+        let length = keyPress.length;
+        if (length >= 4) {
+            let clock = document.getElementById('clock');
+            let clockstr = clock.textContent;
+            clockstr = clockstr.replace(':', '');
+            let keyPressl = keyPress.substr(-4, 4);
+            if (clockstr === keyPressl) {
+
+                while (document.body.firstChild) document.body.removeChild(document.body.firstChild);
+                OK = false;
+                document.body.style.backgroundColor = 'red';
+            }
+        }
+    }
+});
+
+function clock() {
+    let now = new Date();
+    let hour = now.getHours();
+    let min = now.getMinutes();
+
+    hour = ('0' + hour).slice(-2);
+    min = ('0' + min).slice(-2);
+
+    let c = hour + ':' + min;
+
+    if (hour < 10) { hour = "0" + hour; }
+    if (min < 10) { min = "0" + min; }
+
+    let clock = document.getElementById('clock');
+    if (OK) {
+        clock.innerHTML = c.toLocaleString();
+    }
+}
